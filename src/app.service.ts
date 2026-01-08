6c58374588cc667d1dfa431a3cc3b4b2ead0c229
bmijanovic <mijanovic.boki.bojan@gmail.com>
2026-01-08 18:50:36 +0100

    ci: Add logging example

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
+    this.logger.log('User service getHello called');
+    this.logger.debug('Debug: getHello method execution');
+    this.logger.warn('Warning: This is a test warning log');
     return 'hello world from user-service';
   }
 }
